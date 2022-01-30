588e73af4ff96823029a9c83e64510fd74637073
fujichi70 <nyq_o0@yahoo.co.jp>
2022-01-30 16:01:35 +0900

    デザイン修正

--- opening.js
+++ opening.js
@@ -1,22 +1,9 @@
 'use strict';
 
 {
-
     const opening = document.querySelector('.opening');
+    const body = document.querySelector('body');
 
-    const chars = newMe.textContent.trim().split("");
-    const charsSplit = chars.map((val) => {
-      if (val === " ") {
-        val = val.replace(" ", "&nbsp;");
-        return `<span>${val}</span>`;
-      } else {
-        return `<span>${val}</span>`;
-      }
-    });
-  
-    const charsJoin = charsSplit.join("");
-    
-    newMe.innerHTML = charsJoin;
 
     setTimeout(() => {
         opening.classList.add('start');
@@ -27,15 +14,6 @@
         setTimeout(() => {
             opening.classList.add('hide');
         }, 3500);
-    }, 10);
-
-    function charsAnimation() {
-        Array.from(newMe.children).forEach((e, index) => {
-          setTimeout(() => {
-            e.classList.toggle("move");
-          }, 100 * index);
-        });
-      }
-    
+    }, 9);
 
 }
